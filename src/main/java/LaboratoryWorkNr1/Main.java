5c2f933d4690bd33ab07fb4fe38502f41340736c
Damian <damiangrosu3@gmail.com>
2022-09-29 20:34:39 +0300

    added affine and playfair ciphers

--- src/main/java/LaboratoryWorkNr1/Main.java
+++ src/main/java/LaboratoryWorkNr1/Main.java
@@ -1,22 +1,42 @@
 package LaboratoryWorkNr1;
 
+import LaboratoryWorkNr1.ciphers.AffineCipher;
 import LaboratoryWorkNr1.ciphers.CaesarSubstitutionCipher;
+import LaboratoryWorkNr1.ciphers.PlayFairCipher;
 import LaboratoryWorkNr1.ciphers.VigenereCipher;
 
 public class Main {
     public static void main(String[] args) {
+
         //########################################################
         System.out.println("Substitution Caesar Cipher :");
         CaesarSubstitutionCipher cipher1 = new CaesarSubstitutionCipher(4);
-        System.out.println("Encrypted message -> " + cipher1.encrypt("ATTACKATONCE"));
-        System.out.println("Decrypted message -> " + cipher1.decrypt(cipher1.encrypt("ATTACKATONCE")));
+        System.out.println("Encrypted message -> " + cipher1.encrypt("MAGICAL"));
+        System.out.println("Decrypted message -> " + cipher1.decrypt(cipher1.encrypt("MAGICAL")));
         //########################################################
         System.out.println("---------------------------------------------");
         //########################################################
         System.out.println("Vigenere Cipher :");
-        VigenereCipher cipher2 = new VigenereCipher("ANANAS");
+        VigenereCipher cipher2 = new VigenereCipher("HELLO");
         System.out.println("Encrypted message -> " + cipher2.encrypt("BANANA"));
         System.out.println("Decrypted message -> " + cipher2.decrypt(cipher2.encrypt("BANANA")));
-
+        //########################################################
+        System.out.println("---------------------------------------------");
+        System.out.println("PlayFair Cipher :");
+        String textToBeEncrypted = "hi world";
+        String key = "java";
+        PlayFairCipher playFairCipher = new PlayFairCipher();
+        String matrix = playFairCipher.createMatrix(key.replace(" ", ""));
+        String[] pairs = playFairCipher.divideInPairs(textToBeEncrypted.replace("j", "i"));
+        String encryptedMessage = playFairCipher.encrypt(pairs, matrix);
+        System.out.println("Encrypted message -> " + encryptedMessage);
+        System.out.println("Decrypted message -> " + playFairCipher.decrypt(playFairCipher.divideInPairs(encryptedMessage), matrix));
+        //########################################################
+        System.out.println("---------------------------------------------");
+        System.out.println("Affine Cipher :");
+        AffineCipher affineCipher = new AffineCipher(17, 20);
+        String text = "HI WORLD";
+        System.out.println("Encrypted message -> " + affineCipher.encrypt(text));
+        System.out.println("Decrypted message -> " + affineCipher.decrypt(affineCipher.encrypt(text)));
     }
 }
---
+++ src/main/java/LaboratoryWorkNr1/ciphers/AffineCipher.java
@@ -0,0 +1,55 @@
+package LaboratoryWorkNr1.ciphers;
+
+import LaboratoryWorkNr1.Crypting;
+
+public class AffineCipher implements Crypting {
+
+    private final int key1;
+    private final int key2;
+
+    public AffineCipher(int key1, int key2) {
+        this.key1 = key1;
+        this.key2 = key2;
+    }
+
+    @Override
+    public String encrypt(String message) {
+
+        StringBuilder ciphertext = new StringBuilder();
+
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) != ' '){
+                ciphertext.append((char) ((((key1 * (message.charAt(i) - 'A')) + key2) % 26) + 'A'));
+            } else {
+                ciphertext.append(message.charAt(i));
+            }
+        }
+
+        return ciphertext.toString();
+    }
+
+    @Override
+    public String decrypt(String message) {
+
+        StringBuilder encryptedmessage = new StringBuilder();
+        int a_inv = 0;
+        int flag = 0;
+
+        for (int i = 0; i < 26; i++) {
+            flag = (key1 * i) % 26;
+            if (flag == 1) {
+                a_inv = i;
+            }
+        }
+
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) != ' ') {
+                encryptedmessage.append( (char) (((a_inv * ((message.charAt(i) + 'A' - key2)) % 26)) + 'A') );
+            } else {
+                encryptedmessage.append(message.charAt(i));
+            }
+        }
+
+        return encryptedmessage.toString();
+    }
+}
